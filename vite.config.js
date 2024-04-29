01381044447d2dca95592792315cf1d2b274c4a3
Praveen jangir <73075157+praveen-jangir@users.noreply.github.com>
2024-04-29 17:53:49 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import react from '@vitejs/plugin-react'
 //   plugins: [react()],
 // })
 export default defineConfig({
-  base: "/build",
+  base: "/",
   plugins: [react()],
   build: {
     chunkSizeWarningLimit: 1600,
